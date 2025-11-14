1af4beb8e2a6663ad2004dd7c3a12d19f877767c
Dinesh107 <dineshjr107@gmail.com>
2025-11-14 20:35:32 +0530

    fivith commit

---
+++ index.js
@@ -0,0 +1,78 @@
+// console.log("Hello students");
+// console.log(`I love javascript`);
+// console.log('I love javascript');
+
+// window.alert("Hai welocome to javascript qworld");
+
+// document.getElementById("myH1").textContent = "Welcome to javascript world";
+
+// document.getElementById("myP1").textContent = "we are learning javascript";
+
+// let age = 24;
+// let price = 10.44;
+// let grade = 5.5;
+// console.log("your age is", age);
+// console.log(`your are ${age} years old`);
+// console.log(typeof price);
+// console.log(typeof grade);
+
+// let firstName = "hisham";
+// let favFood = "Dosa";
+// let email = "hisha111m@gmail.com";
+
+// console.log(`your name is ${typeof firstName}`);
+// console.log(`you like ${favFood}`);
+// console.log(`your email is ${email}`);
+
+// let online = true;
+// let forSale = true;
+
+// console.log(typeof online);
+// console.log(`Hisham is ${online}`);
+// console.log(`is this car is for sale ${forSale}`);
+
+// let fullName = "Hisham Student";
+// let age = 22;
+// let student = true;
+
+// document.getElementById("myH1").innerHTML = `my name is ${fullName}`;
+// document.getElementById("myH2").innerHTML = `my age is ${age}`;
+// document.getElementById("myH3").innerText = `i am student: ${student}`;
+
+// let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 3;
+// students = students % 2;
+// students = students % 2;
+
+// students += 1;
+// students -= 1;
+// students *= 1;
+// students /= 1;
+// students %= 1;
+
+// students++;
+// students--;
+
+// // operators precidence 
+
+// paraenthis ()
+// exponents 
+
+// multipliaction & division & modulos
+
+// addition & subration 
+
+
+// let result = 1 + 2 * 3 + 4 ** 2;
+// let result = 6 / 2 ** (2 + 5);
+// 1 + 2 * 3 + 16
+// 1 + 6 + 16
+// 23
+// console.log(result);
+
+
